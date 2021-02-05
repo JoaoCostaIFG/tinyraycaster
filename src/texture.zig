48a476bf0063f52b7cdf9613e789d210f513e84f
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-02-05 17:13:39 +0000

    Add: texture file refactor.

---
+++ src/texture.zig
@@ -0,0 +1,91 @@
+const std = @import("std");
+const assert = std.debug.assert;
+const log = std.log;
+
+const c = @import("c.zig");
+const utils = @import("utils.zig");
+
+pub const Texture = struct {
+    w: usize,
+    h: usize,
+    size: usize,
+    cnt: usize,
+    tex: []u32 = undefined,
+
+    pub fn destructor(self: *Texture) void {
+        const allocator = std.heap.page_allocator;
+        allocator.free(self.tex);
+    }
+
+    pub fn get(self: *Texture, x: usize, y: usize, idx: usize) u32 {
+        assert(idx < self.cnt);
+        return self.tex[x + idx * self.size + y * self.w];
+    }
+
+    // TODO txcoord usize instead of isize?
+    pub fn getScaledColumn(self: *Texture, texid: usize, texcoord: usize, column_height: usize) []u32 {
+        assert(texcoord < self.size and texid < self.cnt);
+
+        const allocator = std.heap.page_allocator;
+        var column = allocator.alloc(u32, column_height) catch unreachable;
+
+        var y: usize = 0;
+        while (y < column_height) : (y += 1) {
+            column[y] = self.get(texcoord, (y * self.size) / column_height, texid);
+        }
+        return column;
+    }
+};
+
+// TODO optional pointer?
+pub fn loadTexture(filename: [*:0]const u8) Texture {
+    var nchannels: c_int = -1;
+    var w: c_int = undefined;
+    var h: c_int = undefined;
+    const pixmap: ?[*]u8 = c.stbi_load(filename, &w, &h, &nchannels, 0);
+    if (pixmap == null) {
+        log.err("loadTexture: can't load the texture ({s}).", .{filename});
+        // return false;
+    }
+    defer c.stbi_image_free(pixmap.?);
+
+    if (4 != nchannels) {
+        log.err("loadTexture: the texture must be a 32 bit image ({s}).", .{filename});
+        // return false;
+    }
+
+    const tex_cnt: usize = @intCast(usize, @divTrunc(w, h));
+    const tex_size: usize = @intCast(usize, w) / tex_cnt;
+    if (w != @intCast(usize, h) * tex_cnt) {
+        log.err("loadTexture: the texture file must contain N square textures packed horizontally ({s}).", .{filename});
+        // return false;
+    }
+
+    // read the texture data
+    const allocator = std.heap.page_allocator;
+    var texture = allocator.alloc(u32, @intCast(usize, h * w)) catch {
+        log.err("loadTexture: texture memory allocation failed ({s}).", .{filename});
+        return Texture{ .w = 0, .h = 0, .size = 0, .cnt = 0 };
+    };
+    var i: usize = 0;
+    while (i < h) : (i += 1) {
+        var j: usize = 0;
+        while (j < w) : (j += 1) {
+            const ind: usize = i * @intCast(usize, w) + j;
+            texture[ind] = utils.packColorA(
+                pixmap.?[ind * 4],
+                pixmap.?[ind * 4 + 1],
+                pixmap.?[ind * 4 + 2],
+                pixmap.?[ind * 4 + 3],
+            );
+        }
+    }
+
+    return Texture{
+        .w = @intCast(usize, w),
+        .h = @intCast(usize, h),
+        .size = tex_size,
+        .cnt = tex_cnt,
+        .tex = texture,
+    };
+}
--- src/tinyraycaster.zig
+++ src/tinyraycaster.zig
@@ -4,76 +4,14 @@ const assert = std.debug.assert;
 const log = std.log;
 
 const Framebuffer = @import("framebuffer.zig").Framebuffer;
-const utils = @import("utils.zig");
 const Map = @import("map.zig");
 const Player = @import("player.zig").Player;
+const Texture = @import("texture.zig");
+const utils = @import("utils.zig");
 
 pub extern "c" fn SDL_SetRelativeMouseMode(enabled: c_int) c_int;
 
-fn loadTexture(filename: [*:0]const u8, texture: *[]u32, tex_size: *usize, tex_cnt: *usize) bool {
-    var nchannels: c_int = -1;
-    var w: c_int = undefined;
-    var h: c_int = undefined;
-    const pixmap: ?[*]u8 = c.stbi_load(filename, &w, &h, &nchannels, 0);
-    if (pixmap == null) {
-        log.err("loadTexture: can't load the texture ({s}).", .{filename});
-        return false;
-    }
-    defer c.stbi_image_free(pixmap.?);
-
-    if (4 != nchannels) {
-        log.err("loadTexture: the texture must be a 32 bit image ({s}).", .{filename});
-        return false;
-    }
-
-    tex_cnt.* = @intCast(usize, @divTrunc(w, h));
-    tex_size.* = @intCast(usize, w) / tex_cnt.*;
-    if (w != @intCast(usize, h) * tex_cnt.*) {
-        log.err("loadTexture: the texture file must contain N square textures packed horizontally ({s}).", .{filename});
-        return false;
-    }
-
-    texture.* = std.heap.c_allocator.alloc(u32, @intCast(usize, h * w)) catch {
-        log.err("loadTexture: texture memory allocation failed ({s}).", .{filename});
-        return false;
-    };
-    var i: usize = 0;
-    while (i < h) : (i += 1) {
-        var j: usize = 0;
-        while (j < w) : (j += 1) {
-            const ind: usize = i * @intCast(usize, w) + j;
-            texture.*[ind] = utils.packColorA(pixmap.?[ind * 4], pixmap.?[ind * 4 + 1], pixmap.?[ind * 4 + 2], pixmap.?[ind * 4 + 3]);
-        }
-    }
-
-    return true;
-}
-
-// TODO txcoord usize instead of isize?
-fn textureColumn(img: *[]u32, texsize: usize, ntextures: usize, texid: usize, texcoord: usize, column_height: usize) *[]u32 {
-    const img_w: usize = texsize * ntextures;
-    const img_h: usize = texsize;
-    assert(texcoord < texsize and texid < ntextures);
-
-    var column = std.heap.c_allocator.alloc(u32, column_height) catch unreachable;
-
-    var y: usize = 0;
-    while (y < column_height) : (y += 1) {
-        const pix_x: usize = texid * texsize + texcoord;
-        const pix_y: usize = (y * texsize) / column_height;
-        column[y] = img.*[pix_x + pix_y * img_w];
-    }
-    return &column;
-}
-
 pub fn main() !u8 {
-    var walltex: []u32 = undefined;
-    var walltex_size: usize = undefined;
-    var walltex_cnt: usize = undefined;
-    if (!loadTexture("assets/walltext.png", &walltex, &walltex_size, &walltex_cnt))
-        return 1;
-    defer std.heap.c_allocator.free(walltex);
-
     const win_w: usize = 1024; // image width
     const win_h: usize = 512; // image height
     // the image itself, initialized to white
@@ -84,6 +22,9 @@ pub fn main() !u8 {
     try framebuffer.init();
     defer framebuffer.destructor();
     framebuffer.clear(utils.packColor(255, 255, 255));
+    // load textures
+    var walltex = Texture.loadTexture("assets/walltext.png");
+    defer walltex.destructor();
     // read map
     var map = Map.readMap();
     defer map.destructor();
@@ -109,8 +50,7 @@ pub fn main() !u8 {
                 const rect_x: usize = j * rect_w;
                 const rect_y: usize = i * rect_h;
                 const texid: u32 = map.get(j, i);
-                assert(texid < walltex_cnt);
-                framebuffer.drawRectangle(rect_x, rect_y, rect_w, rect_h, walltex[texid * walltex_size]);
+                framebuffer.drawRectangle(rect_x, rect_y, rect_w, rect_h, walltex.get(0, 0, texid));
             }
         }
     }
@@ -141,23 +81,22 @@ pub fn main() !u8 {
                 const map_y: usize = @floatToInt(usize, cy);
                 if (!map.isEmpty(map_x, map_y)) { // hit obstacle
                     const texid = map.get(map_x, map_y);
-                    assert(texid < walltex_cnt);
                     const column_height: usize = @floatToInt(usize, @intToFloat(f32, win_h) / (t * @cos(angle - player.angle)));
 
                     const hitx: f32 = cx - @floor(cx + 0.5); // hitx and hity contain (signed) fractional parts of cx and cy,
                     const hity: f32 = cy - @floor(cy + 0.5); // between [-0.5, 0.5], one is very close to 0
-                    var x_texcoord: isize = @floatToInt(isize, hitx * @intToFloat(f32, walltex_size));
+                    var x_texcoord: isize = @floatToInt(isize, hitx * @intToFloat(f32, walltex.size));
 
                     // we need to determine whether we hit a "vertical" or
                     // a "horizontal" wall (w.r.t the map)
                     if (@fabs(hity) > @fabs(hitx))
-                        x_texcoord = @floatToInt(isize, hity * @intToFloat(f32, walltex_size));
+                        x_texcoord = @floatToInt(isize, hity * @intToFloat(f32, walltex.size));
                     if (x_texcoord < 0)
-                        x_texcoord += @intCast(isize, walltex_size); // do not forget x_texcoord can be negative, fix that
+                        x_texcoord += @intCast(isize, walltex.size); // do not forget x_texcoord can be negative, fix that
 
                     // draw textured wall
-                    const column: *[]u32 = textureColumn(&walltex, walltex_size, walltex_cnt, texid, @intCast(usize, x_texcoord), column_height);
-                    defer std.heap.c_allocator.free(column.*);
+                    const column: []u32 = walltex.getScaledColumn(texid, @intCast(usize, x_texcoord), column_height);
+                    defer std.heap.page_allocator.free(column);
 
                     pix_x = win_w / 2 + i;
                     var j: usize = 0;
@@ -165,7 +104,7 @@ pub fn main() !u8 {
                         pix_y = j + win_h / 2 - column_height / 2;
                         if (pix_y < 0 or pix_y >= win_h)
                             continue;
-                        framebuffer.setPixel(pix_x, pix_y, column.*[j]);
+                        framebuffer.setPixel(pix_x, pix_y, column[j]);
                     }
 
                     break;
