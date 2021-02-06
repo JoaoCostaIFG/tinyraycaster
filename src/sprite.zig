a73a8dcb4a18b884a31e3244b159cca5bc7b246b
JoaoCostaIFG <joaocosta.work@posteo.net>
2021-02-06 14:08:33 +0000

    Add: player dist and comparator function for Sprites.

--- src/sprite.zig
+++ src/sprite.zig
@@ -2,4 +2,9 @@ pub const Sprite = struct {
     x: f32,
     y: f32,
     tex_id: usize,
+    player_dist: f32 = 0.0,
+
+    pub fn isCloser(self: *Sprite, o: *Sprite) bool {
+        return self.player_dist < o.player_dist;
+    }
 };
